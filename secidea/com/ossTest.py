87084647bfeb4fe6deea204a21cf58bcc97ea9e3
Idoit-z <xiaozhou.k@foxmail.com>
2020-10-20 17:15:59 +0800

    aa
    aaa

---
+++ secidea/com/ossTest.py
@@ -0,0 +1,13 @@
+"""
+CompanyDomainName:secidea.com
+
+"""
+def test():
+    num=1
+    if num==1:
+        print('oss')
+<<<<<<< HEAD:ossTest.php
+		print('host')
+=======
+        print('host')
+>>>>>>> b6723f5bb436ab410c938cfe9d2e1000413e164a:ossTest.py
